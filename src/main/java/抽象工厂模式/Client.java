56cb9ba6a91c43f3cbe13b50d2784234b5c2a8f4
hao <haoaying@outlook.com>
2019-12-05 17:08:40 +0800

    update 抽象工厂模式

---
+++ src/main/java/抽象工厂模式/Client.java
@@ -0,0 +1,61 @@
+package 抽象工厂模式;
+
+public class Client {
+}
+
+
+//抽象产品A
+abstract class AbstractProductA {
+    public void shareMethod() {
+
+    }
+
+    public abstract void doSomething();
+}
+
+
+//具体产品
+class ProductA1 extends AbstractProductA {
+    public void doSomething() {
+        System.out.println("生产产品a1");
+    }
+}
+
+class ProductA2 extends AbstractProductA {
+    public void doSomething() {
+        System.out.println("生产产品a2");
+    }
+}
+
+
+//抽象产品B
+abstract class AbstractProductB {
+    public void shareMethod() {
+
+    }
+
+    public abstract void doSomething();
+}
+
+
+//具体产品
+class ProductB1 extends AbstractProductB {
+    public void doSomething() {
+        System.out.println("生产产品B1");
+    }
+}
+
+class ProductB2 extends AbstractProductB {
+    public void doSomething() {
+        System.out.println("生产产品B2");
+    }
+}
+
+
+//抽象工厂类
+abstract class AbstractCreator {
+    public abstract AbstractProductA createProductA();
+
+    public abstract AbstractProductB createProductB();
+}
+
